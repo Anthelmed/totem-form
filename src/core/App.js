69229a47848ced259a6acca32b8c8406548c4bfe
anthelme_dumont <anthelme.dumont@gmail.com>
2016-11-15 23:52:06 +0100

    kute js svg morphing

--- src/core/App.js
+++ src/core/App.js
@@ -9,7 +9,26 @@ class App {
      * begin method
      */
     static begin() {
-        // Section 2 / Menu
+
+        const sections = document.querySelectorAll('[id^="section-"]');
+
+        for (let i = 0; i < sections.length; i++) {
+            const section = sections[i];
+            let index = section.getAttribute('data-index');
+
+            section.addEventListener('click', () => {
+                section.classList.remove('visible');
+                KUTE.to('#path-beforeMorph-'+ index, {
+                    path: '#path-step-1-' + index
+                }, {
+                    easing: 'easingCubicInOut',
+                    duration: 500
+                }).start();
+            });
+            section.style.zIndex = sections.length - i;
+        }
+
+        /*// Section 2 / Menu
         const section2DraggableElement = document.querySelector('#section-2 .draggable');
         const section2Bounds = document.querySelector('#section-2 .drag-wrapper');
         const section2Targets = document.querySelectorAll('#section-2 [class^="drag-target"]');
@@ -21,7 +40,7 @@ class App {
         const section4Bounds = document.querySelector('#section-4 .drag-wrapper');
         const section4Targets = document.querySelectorAll('#section-4 [class^="drag-target"]');
 
-        const section4DragAndDrop = new DragAndDrop(section4DraggableElement, section4Bounds, section4Targets);
+        const section4DragAndDrop = new DragAndDrop(section4DraggableElement, section4Bounds, section4Targets);*/
     }
 }
 
